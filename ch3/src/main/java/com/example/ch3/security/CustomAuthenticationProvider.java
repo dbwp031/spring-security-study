b4478f7fd3a25f929eb6317c621a6a85e8dcf6e9
Yuje Lee <dbwp031@khu.ac.kr>
2023-06-01 20:05:58 +0900

    update ch3

---
+++ ch3/src/main/java/com/example/ch3/security/CustomAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package com.example.ch3.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private UserDetailsService userDetailsService; // 사용자 찾는 책임
+
+    @Autowired
+    private PasswordEncoder passwordEncoder; // 인증 프로세스에서 암호를 관리
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails u = userDetailsService.loadUserByUsername(username); // UserDetailsService에서 username을 가지고 있는 유저가 있는지 찾음
+        if (passwordEncoder.matches(password, u.getPassword())) { // passwordEncoder가 암호 관리
+            return new UsernamePasswordAuthenticationToken(username, password, u.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Something went wrong!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
